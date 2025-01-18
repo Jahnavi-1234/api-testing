9558870757261dfde3b23b9c550b2402a4d48a45
Jahnavi-1234 <jnimmakayala@griddynamics.com>
2025-01-18 12:17:27 +0530

    Update constant_values.py
    Token removed

--- constant_values.py
+++ constant_values.py
@@ -1,5 +1,5 @@
 # constant_values.py
 # This file contains the constants required to interact with the Trello API.
-TOKEN = "ATTAed7c387e59facf74ef624c708f92117905142ec2b9cc4f5273cd364576e159cc9DE87560"
+TOKEN = {TOKEN}
 BASE_URL = "https://api.trello.com/1"
 BOARD_ID = f"{BASE_URL}/members/me/boards?key={API_KEY}&token={TOKEN}"
